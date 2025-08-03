1b984f8d59c6bb37889817844b6105ccab53acc6
juniormesquitadandao <juniormesquitadandao@gmail.com>
2025-08-02 22:16:56 -0300

    terminal

--- front/src/util/view/home/index_view.js
+++ front/src/util/view/home/index_view.js
@@ -35,8 +35,8 @@ export default class IndexView extends ApplicationView {
 
   terminal = (_) => (
     <div className='w-full h-screen bg-black text-green-400 font-mono flex flex-col'>
-      <h1 className='text-center text-2xl py-8 flex items-center justify-center gap-3'>
-        <img src='/favicon.svg' alt='AI4COD' width='32' height='32' />
+      <h1 className='text-center text-4xl py-8 flex items-center justify-center gap-3'>
+        <img src='/favicon.svg' alt='AI4COD' width='64' height='64' />
         AI for Code
       </h1>
 
